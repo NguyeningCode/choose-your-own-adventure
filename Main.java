7ae93164c5b128f13e8a7cadfeac5a10c040bd00
jnguyen7410 <jnguyen7410@gmail.com>
2020-08-13 17:05:25 -0700

    Adding logic for next class

--- Main.java
+++ Main.java
@@ -1,25 +1,52 @@
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Main {
+
+    static Scanner input = new Scanner(System.in);
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
+        int answer;
         StoryList storyList = new StoryList();
         boolean isFinished = false;
-        Story currentStory = storyList.getStory(0);
+        Story currentStory = storyList.getStory(1);
         while (!isFinished) {
             currentStory.printStoryAndOptions();
-            int answer = input.nextInt();
+            answer = readInput();
+            
             if (currentStory.getStoryFinished() && answer == 2) {
                 isFinished = true;
             }
+            else if (currentStory.getStoryFinished() && answer == 1) {
+                currentStory = storyList.getStory(1);
+            }
             else if (answer == 1) {
-                currentStory = currentStory.choice1Story != null ? currentStory.choice1Story : storyList.getStory(99);
+                currentStory = storyList.getStory(currentStory.getChoice1Story());
             } else {
-                currentStory = currentStory.choice2Story != null ? currentStory.choice2Story : storyList.getStory(99);
+                currentStory = storyList.getStory(currentStory.getChoice2Story());
             }
         }
         
         input.close();
+    }
+
+    public static int readInput() {
+        
+        int answer = 0;
+        do {
+            try{
+                String temp = input.nextLine();
+                answer = Integer.parseInt(temp);
+                if (answer != 1 && answer != 2) {
+                    System.out.println();
+                    System.out.print("That is not a valid input! Please choose between 1 and 2: ");    
+                }
+            } catch (Exception e) {
+                System.out.println();
+                System.out.print("That is not a valid input! Please choose between 1 and 2: ");
+            }
+            
+        } while (answer != 1 && answer != 2);
+        return answer;
     }
 
     
--- StoryList.java
+++ StoryList.java
@@ -1,4 +1,12 @@
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
 public class StoryList {
+    private static final Pattern COMMA_DELIMITER = Pattern.compile(",");
     Story[] stories;
 
     public StoryList(Story[] stories) {
@@ -10,24 +18,75 @@ public class StoryList {
     }
 
     public Story getStory(int num) {
-        return stories[num] != null ? stories[num] : stories[99];
+        return stories[num];
     }
 
     public void initStories() {
         // set aside some space
         stories = new Story[100];
+
+        // List<List<String>> records = new ArrayList<>();
+        // try (Scanner scanner = new Scanner(new File("story.csv"));) {
+        //     // do this to skip over the first line
+        //     scanner.nextLine();
+        //     while (scanner.hasNextLine()) {
+        //         // records.add(getRecordFromLine(scanner.nextLine()));
+        //         List<String> storyValues = getRecordFromLine(scanner.nextLine());
+        //         int storyIndex = Integer.parseInt(storyValues.get(0));
+        //         stories[storyIndex] = parseStory(storyIndex, storyValues);
+        //     }
+        // } catch (Exception e) {
+        //     System.out.println("Error while reading file: " + e.getMessage());
+        //     System.exit(1);
+        // }
+    // }
         
         // create our stories
-        Story story1 = new Story("Once upon a time, some rabbit did drugs. Suddenly, he locked eyes with you. What do you do?", "Hit him", "Chill and do drugs with him");
-        Story story2 = new Story("You both hit a bong, it was great. Now as you're starting to pass out, the rabbit runs away with your wallet. What do?", "Chase after him", "Go to sleep");
-        Story end = new Story("You died.");
+        Story story1 = new Story(1, "Once upon a time, some rabbit did drugs. Suddenly, he locked eyes with you. What do you do?", "Hit him", "Chill and do drugs with him");
+        Story story2 = new Story(2, "You both hit a bong, it was great. Now as you're starting to pass out, the rabbit runs away with your wallet. What do?", "Chase after him", "Go to sleep");
+        Story end = new Story(0, "You died.");
         
         // create the relationships here
-        story1.setChoice2Story(story2);
+        story1.setChoice2Story(2);
 
-        stories[0] = story1;
-        stories[1] = story2;
-        stories[99] = end;
+        stories[1] = story1;
+        stories[2] = story2;
+        stories[0] = end;
+        }
+
+    private List<String> getRecordFromLine(String line) {
+        List<String> values = new ArrayList<String>();
+        try (Scanner rowScanner = new Scanner(line)) {
+            while (rowScanner.hasNext()) {
+                values = Arrays.asList(rowScanner.next().split(","));
+            }
+        }
+        return values;
     }
 
+    private Story parseStory(int storyIndex, List<String> storyValues) {
+        Story story = null;
+        if (storyIndex >= 0 && storyIndex <= 99) {
+            if (storyValues.get(2) == null && storyValues.get(3) == null) {
+                story = new Story(storyIndex, storyValues.get(1));
+            } else if (storyValues.get(3) == null) {
+                story = new Story(storyIndex, storyValues.get(1), storyValues.get(2));
+                if (storyValues.get(4) != null) {
+                    int choice1 = Integer.parseInt(storyValues.get(4));
+                    story.setChoice1Story(choice1);
+                }
+            } else {
+                story = new Story(storyIndex, storyValues.get(1), storyValues.get(2), storyValues.get(3));
+                if (storyValues.get(4) != null) {
+                    int choice1 = Integer.parseInt(storyValues.get(4));
+                    story.setChoice1Story(choice1);
+                }
+                if (storyValues.get(5) != null) {
+                    int choice2 = Integer.parseInt(storyValues.get(5));
+                    story.setChoice1Story(choice2);
+                }
+            }    
+        }
+        return story;
+    }
 }
